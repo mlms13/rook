c00b9ef3be7cd5ab460dfc036e75cfec3d857ca3
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-12-30 16:37:04 -0500

    Return the full card constructor
    This way we can require the whole constructor and easily create lots of
    cards.

--- js/modules/card.js
+++ js/modules/card.js
@@ -1,27 +1,26 @@
 define(function () {
-    return {
-        power: 0,
-        worth: 0,
-        suit: undefined,
-        create: function (suit, power, worth, face) {
-            this.suit = suit; // red, green, yellow, or black
-            this.worth = worth; // how many points the cord is worth when won
-            this.power = power; // the card's strength vs other cards
-            this.face = face; // the number show on the card (usually the same as the power)
-        },
-        render: function () {
-            // create and return a jQuery object representing the card
-            var $card = $('<div class="rook-card" />');
+    // a private template for a new card object
+    function Card (suit, power, worth, face) {
+        this.suit = suit; // red, green, yellow, or black
+        this.worth = worth; // how many points the cord is worth when won
+        this.power = power; // the card's strength vs other cards
+        this.face = face || power; // the number show on the card (usually the same as the power)
+    }
 
-            // add worth, if the card has worth
-            if (this.worth > 0) {
-                $card.attr('data-card-worth', this.worth);
-            }
+    Card.prototype.render = function () {
+        // create and return a jQuery object representing the card
+        var $card = $('<div class="rook-card" />');
 
-            return $card.addClass(this.suit)
-                .attr('data-card-suit', this.suit)
-                .attr('data-card-face', this.face)
-                .append($('<span class="rook-card-inner">' + this.face + '</span>'));
+        // add worth, if the card has worth
+        if (this.worth > 0) {
+            $card.attr('data-card-worth', this.worth);
         }
-    };
+
+        return $card.addClass(this.suit)
+            .attr('data-card-suit', this.suit)
+            .attr('data-card-face', this.face)
+            .append($('<span class="rook-card-inner">' + this.face + '</span>'));
+    }
+
+    return Card;
 });
