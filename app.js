2ecd9a23bee9501d4eeb4e917f46b66cb0a0e103
Michael Martin-Smucker <mlmartin13@gmail.com>
2013-12-31 17:40:34 -0500

    Add socket.io
    Magicness to come?

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var io = require('socket.io');
 
-var app = express();
+var app = express(),
+    server = require('http').createServer(app);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -28,11 +30,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+io = io.listen(server);
+
+server.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
