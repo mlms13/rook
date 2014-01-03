5d57e6ebb68a030fc2097e770f60c5645ff47f50
Michael Martin-Smucker <mlmartin13@gmail.com>
2014-01-03 10:40:54 -0500

    Make sure the socket connects
    ...before setting up the lobby

--- public/js/modules/lobby.js
+++ public/js/modules/lobby.js
@@ -1,26 +1,32 @@
 define(['modules/socket'], function (sio) {
-    var socket = sio.socket;
+    var socket = sio.socket,
+        lobby = {};
 
-    return {
-        create: function () {
-            var $username = $('#join-lobby-username'),
-                $form = $('#join-lobby-form');
+    lobby.create = function () {
+        var $username = $('#join-lobby-username'),
+            $form = $('#join-lobby-form');
 
-            // let the server know we're joining
-            $form.on('submit', function () {
-                socket.emit('join game', {username: $username.val()});
-                return false; // prevent the form from submitting
-            });
-
-            socket.on('lobby changed', function (data) {
-                console.log(data.name, 'has joined, brining the number of members in the room to', data.count);
-            });
-
-            // handle the dealing of cards
-            socket.on('start game', function (data) {
-                console.log('The room is full, and a game is about to begin.');
-            });
+        // make sure our socket is connected
+        if (!socket.socket.connected) {
+            socket.on('connect', function () { lobby.create(); });
+            return false;
         }
-    }
-    
+
+        // let the server know we're joining
+        $form.on('submit', function () {
+            socket.emit('join game', {username: $username.val()});
+            return false; // prevent the form from submitting
+        });
+
+        socket.on('lobby changed', function (data) {
+            console.log(data.name, 'has joined, brining the number of members in the room to', data.count);
+        });
+
+        // handle the dealing of cards
+        socket.on('start game', function (data) {
+            console.log('The room is full, and a game is about to begin.');
+        });
+    };
+
+    return lobby;
 });
